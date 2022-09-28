044b489dc40aa1e5c55f531ced37f08ad16a885d
vimaltripathi98 <79628512+vimaltripathi98@users.noreply.github.com>
2022-09-28 23:06:34 +0530

    src commit

---
+++ src/components/about.jsx
@@ -0,0 +1,40 @@
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            <img src="img/intro-bg1.jpg" className="img-responsive" alt="" />
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>About Us</h2>
+              <h3>{props.data ? props.data.paragraph : "loading..."}</h3>
+              <h3>Why Choose Us?</h3>
+              <div className="list-style">
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> {d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
